4e45981fab0b5a20089fa8efe8ff7972ba148ae3
Masahiro Nawata <kamo.tanabota@gmail.com>
2012-03-16 15:03:16 +0900

    整形

--- property_map/vector_property_map.hpp
+++ property_map/vector_property_map.hpp
@@ -1,8 +1,8 @@
 #ifndef CANARD_VECOTOR_PROPERTY_MAP_HPP
 #define CANARD_VECOTOR_PROPERTY_MAP_HPP
 
-#include <boost/property_map/property_map.hpp>
 #include <vector>
+#include <boost/property_map/property_map.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/type_traits/is_const.hpp>
@@ -14,36 +14,60 @@ namespace Canard {
                 typename std::iterator_traits<
                     typename std::vector<T, Alloc>::iterator
                 >::reference,
-                vector_property_map<T, IndexMap, Alloc> >
+                vector_property_map<T, IndexMap, Alloc>
+             >
   {
     typedef std::vector<T, Alloc> Storage;
     typedef typename boost::property_traits<IndexMap>::value_type index_type;
 
   public:
     typedef T value_type;
-    typedef typename boost::property_traits<IndexMap>::key_type key_type;
     typedef typename std::iterator_traits<
         typename std::vector<T, Alloc>::iterator
       >::reference reference;
+    typedef typename boost::property_traits<IndexMap>::key_type key_type;
     typedef typename boost::mpl::if_<
       boost::is_const<T>,
       boost::readable_property_map_tag,
-      boost::lvalue_property_map_tag>::type category;
+      boost::lvalue_property_map_tag
+    >::type category;
 
   public:
-    explicit vector_property_map(IndexMap const& i_map = IndexMap())
-      : store_(boost::make_shared<Storage>()), index_map_(i_map)
-    { }
+    vector_property_map()
+      : store_(boost::make_shared<Storage>()),
+        index_map_(IndexMap())
+    {
+    }
 
-    vector_property_map
-    (std::size_t initial_size, IndexMap const& i_map = IndexMap())
-      : store_(boost::make_shared<Storage>(initial_size)), index_map_(i_map)
-    { }
+    explicit vector_property_map(IndexMap const& i_map)
+      : store_(boost::make_shared<Storage>()),
+        index_map_(i_map)
+    {
+    }
 
-    void resize(std::size_t size)
-    { store_->resize(size); }
+    explicit vector_property_map(std::size_t initial_size)
+      : store_(boost::make_shared<Storage>(initial_size)),
+        index_map_(IndexMap())
+    {
+    }
 
-    reference operator[](const key_type& key) const
+    vector_property_map(std::size_t initial_size, IndexMap const& i_map)
+      : store_(boost::make_shared<Storage>(initial_size)),
+        index_map_(i_map)
+    {
+    }
+
+    void resize(std::size_t new_size)
+    {
+      store_->resize(new_size);
+    }
+
+    void resize(std::size_t new_size, value_type const& value)
+    {
+      store_->resize(new_size, value);
+    }
+
+    reference operator[](key_type const& key) const
     {
       index_type const i = get(index_map_, key);
       if (static_cast<std::size_t>(i) >= store_->size()) {
