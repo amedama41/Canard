ad9404c3fb6a82799dbd9cd494593859e71c5256
Masahiro Nawata <kamo.tanabota@gmail.com>
2012-03-16 14:53:55 +0900

    引数として受け取るkeyの型をconst参照に変更

--- property_map/logical_property_map.hpp
+++ property_map/logical_property_map.hpp
@@ -1,74 +1,111 @@
-#ifndef CANARD_PROPERTY_MAP_H
-#define CANARD_PROPERTY_MAP_H
+#ifndef CANARD_LOGICAL_PROPERTY_MAP_H
+#define CANARD_LOGICAL_PROPERTY_MAP_H
 
 #include <boost/property_map/property_map.hpp>
 
 namespace Canard {
   template <class PredictMap>
-    struct not_predict_map
-      : public boost::put_get_helper<bool, not_predict_map<PredictMap> >
+  class not_predict_map
+    : public boost::put_get_helper<bool, not_predict_map<PredictMap> >
+  {
+  public:
+    typedef typename PredictMap::key_type key_type;
+    typedef bool value_type;
+    typedef bool reference;
+    typedef boost::readable_property_map_tag category;
+
+    explicit not_predict_map(PredictMap const& pmap)
+      : pmap(pmap)
     {
-      typedef bool value_type;
-      typedef bool reference;
-      typedef typename PredictMap::key_type key_type;
-      typedef boost::readable_property_map_tag category;
-      not_predict_map(PredictMap pmap) : pmap(pmap) { }
-      reference operator[](key_type key) const
-      { return !get(pmap, key); }
-      PredictMap pmap;
-    };
+    }
+
+    reference operator[](key_type const& key) const
+    {
+      return !get(pmap, key);
+    }
+
+  private:
+    PredictMap pmap;
+  };
 
   template <class PredictMap>
-    inline not_predict_map<PredictMap>
-    make_not_predict_map(PredictMap pmap)
-    { return not_predict_map<PredictMap>(pmap); }
+  inline not_predict_map<PredictMap>
+  make_not_predict_map(PredictMap const& pmap)
+  {
+    return not_predict_map<PredictMap>(pmap);
+  }
+
 
   template <class PredictMap1, class PredictMap2>
-    struct and_predict_map
-      : public boost::put_get_helper<
-          bool, and_predict_map<PredictMap1, PredictMap2>
-        >
+  class and_predict_map
+    : public boost::put_get_helper<
+        bool,
+        and_predict_map<PredictMap1, PredictMap2>
+      >
+  {
+  public:
+    typedef boost::readable_property_map_tag category;
+    typedef bool value_type;
+    typedef bool reference;
+    typedef typename PredictMap1::key_type key_type;
+
+    and_predict_map(PredictMap1 const& pmap1, PredictMap2 const& pmap2)
+      : pmap1(pmap1), pmap2(pmap2)
     {
-      typedef bool value_type;
-      typedef bool reference;
-      typedef typename PredictMap1::key_type key_type;
-      typedef boost::readable_property_map_tag category;
-      and_predict_map(PredictMap1 pmap1, PredictMap2 pmap2)
-        : pmap1(pmap1), pmap2(pmap2) { }
-      reference operator[](key_type key) const
-      { return get(pmap1, key) && get(pmap2, key); }
-      PredictMap1 pmap1;
-      PredictMap2 pmap2;
-    };
+    }
 
-  template <class PredictMap1, class PredictMap2>
-    inline and_predict_map<PredictMap1, PredictMap2>
-    make_and_predict_map(PredictMap1 pmap1, PredictMap2 pmap2)
-    { return and_predict_map<PredictMap1, PredictMap2>(pmap1, pmap2); }
-
-  template <class PredictMap1, class PredictMap2>
-    struct or_predict_map
-      : public boost::put_get_helper<
-          bool, or_predict_map<PredictMap1, PredictMap2>
-        >
+    reference operator[](key_type const& key) const
     {
-      typedef bool value_type;
-      typedef bool reference;
-      typedef typename PredictMap1::key_type key_type;
-      typedef boost::readable_property_map_tag category;
-      or_predict_map(PredictMap1 pmap1, PredictMap2 pmap2)
-        : pmap1(pmap1), pmap2(pmap2) { }
-      reference operator[](key_type key) const
-      { return get(pmap1, key) || get(pmap2, key); }
-      PredictMap1 pmap1;
-      PredictMap2 pmap2;
-    };
+      return get(pmap1, key) && get(pmap2, key);
+    }
+
+  private:
+    PredictMap1 pmap1;
+    PredictMap2 pmap2;
+  };
 
   template <class PredictMap1, class PredictMap2>
-    inline or_predict_map<PredictMap1, PredictMap2>
-    make_or_predict_map(PredictMap1 pmap1, PredictMap2 pmap2)
-    { return or_predict_map<PredictMap1, PredictMap2>(pmap1, pmap2); }
+  inline and_predict_map<PredictMap1, PredictMap2>
+  make_and_predict_map(PredictMap1 const& pmap1, PredictMap2 const& pmap2)
+  {
+    return and_predict_map<PredictMap1, PredictMap2>(pmap1, pmap2);
+  }
+
+  template <class PredictMap1, class PredictMap2>
+  class or_predict_map
+    : public boost::put_get_helper<
+        bool,
+        or_predict_map<PredictMap1, PredictMap2>
+      >
+  {
+  public:
+    typedef boost::readable_property_map_tag category;
+    typedef bool value_type;
+    typedef bool reference;
+    typedef typename PredictMap1::key_type key_type;
+
+    or_predict_map(PredictMap1 const& pmap1, PredictMap2 const& pmap2)
+      : pmap1(pmap1), pmap2(pmap2)
+    {
+    }
+
+    reference operator[](key_type const& key) const
+    {
+      return get(pmap1, key) || get(pmap2, key);
+    }
+
+  private:
+    PredictMap1 pmap1;
+    PredictMap2 pmap2;
+  };
+
+  template <class PredictMap1, class PredictMap2>
+  inline or_predict_map<PredictMap1, PredictMap2>
+  make_or_predict_map(PredictMap1 const& pmap1, PredictMap2 const& pmap2)
+  {
+    return or_predict_map<PredictMap1, PredictMap2>(pmap1, pmap2);
+  }
 } // namespace Canard
 
-#endif  // CANARD_PROPERTY_MAP_H
+#endif  // CANARD_LOGICAL_PROPERTY_MAP_H
 
